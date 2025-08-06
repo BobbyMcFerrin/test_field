30baea2b92ce585b4df03ec1054228f835040a0f
Bobby McFerrin <Bobby@McFerrin.com>
2025-08-06 16:58:47 +0300

    wip

---
+++ Code/hello.py
@@ -0,0 +1,9 @@
+def my_hello(my_string_to_print):
+    """
+    My Hello
+    :param string_to_print:
+    :return:
+    """
+    print("This is my string: {}".format(my_string_to_print))
+
+my_hello()
